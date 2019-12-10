62600596d0d436717ad5e7080c163185ef1320b6
adharbert <adharbert@gmail.com>
2019-12-10 09:15:17 -0500

    adding new methods

--- server/data/userDts.js
+++ server/data/userDts.js
@@ -1,14 +1,39 @@
 const faker = require('faker');
-let nextId = 0
-class UserModel {
+let nextId = 1
+class UserModel {    
     constructor(firstName, lastName, phone, state) {
         this.firstName = firstName
         this.lastName = lastName
         this.phone = phone
         this.state = state
+        this.ContractId = getLetter() + getNumber()
         this.id = nextId++
         userModels.push(this)
+    
     }
+}
+
+function getNumber() {
+    let result = Math.floor(Math.random()*9000000) + 1000000;
+    return result;
+}
+
+function getLetter() {
+   return Math.random().toString(36).substring(2, 3).toUpperCase();
+}
+
+function generate(n) {
+    var add = 1, max = 12 - add;   // 12 is the min safe number Math.random() can generate without it starting to pad the end with zeros.   
+
+    if ( n > max ) {
+            return generate(max) + generate(n - max);
+    }
+
+    max        = Math.pow(10, n+add);
+    var min    = max/10; // Math.pow(10, n) basically
+    var number = Math.floor( Math.random() * (max - min + 1) ) + min;
+
+    return ("" + number).substring(add); 
 }
 
 // class UserRepository {
@@ -26,6 +51,7 @@ class UserModel {
 //module.exports = new UserRepository()
 
 let userModels = []
+let AccountDetails = {}
 const createFake = () => new UserModel(faker.name.firstName(), faker.name.lastName(), faker.phone.phoneNumber(), faker.address.stateAbbr())
 const getAll = () => userModels
 const getById = (id) => userModels.find(u => u.id === id)
